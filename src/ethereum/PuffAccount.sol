8e30a1cf571a6968c5fffa1876984e9f784e805a
kartik_giri <kartikgiri1t30@gmail.com>
2024-07-09 14:45:58 +0530

    first commit

---
+++ src/ethereum/PuffAccount.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {IAccount} from "account-abstraction/interfaces/IAccount.sol";
+import {PackedUserOperation} from "account-abstraction/interfaces/PackedUserOperation.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
+
+contract PuffAccount is IAccount, Ownable {
+
+    constructor(address entryPoint) Ownable(msg.sender) {}
+
+    // A signature is valid, if it's the MinimalAccount owner
+    function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
+        external
+        returns (uint256 validationData)
+    {
+        validationData = _validateSignature(userOp, userOpHash);
+        // _validateNonce()
+        // _payPrefund(missingAccountFunds);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                       INTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    // EIP-191 version of the signed hash
+    function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash)
+        internal
+        view
+        returns (uint256 validationData)
+    {
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+        address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
+        if (signer != owner()) {
+            return SIG_VALIDATION_FAILED;
+        }
+        return SIG_VALIDATION_SUCCESS;
+    }
+}
